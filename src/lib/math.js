ac4fe562c8b73343cf6cd5e281c10527c1250e3e
Rafael "Lestat" Kuhn <rafa.faelk@gmail.com>
2025-02-22 15:02:04 -0300

    feature: first iteration of algorithm, finding closest points to drones

--- src/lib/math.js
+++ src/lib/math.js
@@ -1,4 +1,54 @@
 
+
+const infinity = 9_999_999;
+
+export const isApprox = (v, dest) => isApproxThreshold(v, dest, 0.005);
+export const isApproxThreshold = (v, dest, threshold) => Math.abs(v - dest) < threshold;
+
+
+export const euclideanDistance = (x0, y0, x1, y1) => {
+	const pointsVecX = x1 - x0;
+	const pointsVecY = y1 - y0;
+	return Math.sqrt(pointsVecX * pointsVecX + pointsVecY * pointsVecY);
+}
+
+export const euclideanDistanceSquared = (x0, y0, x1, y1) => {
+	const pointsVecX = x1 - x0;
+	const pointsVecY = y1 - y0;
+	return pointsVecX * pointsVecX + pointsVecY * pointsVecY;
+}
+
+export const coord2dToLin = (x, y, width) => y * width + x;
+
+/**
+ * @param {{ x: Number, y: Number }} point0 
+ * @param {{ x: Number, y: Number }} point1 
+ */
+const euclideanDistance2 = (point0, point1) => {
+	const pointsVecX = point1.x - point0.x;
+	const pointsVecY = point1.y - point0.y;
+	return Math.sqrt(pointsVecX * pointsVecX + pointsVecY * pointsVecY);
+}
+
+/**
+ * @param {{ x: Number, y: Number }} point0 
+ * @param {{ x: Number, y: Number }} point1 
+ */
+export const manhattanDistance = (point0, point1) => {
+	return Math.abs(point0.x - point1.x) + Math.abs(point0.y - point1.y);
+}
+
+/**
+ * @param {{ x: Number, y: Number }} point0 
+ * @param {{ x: Number, y: Number }} point1 
+ */
+export const chebyshevDistance = (point0, point1) => {
+	const xDist = Math.abs(point0.x - point1.x);
+	const yDist = Math.abs(point0.y - point1.y);
+	return Math.max(xDist, yDist);
+}
+
+
 /** @return {Number} */
 export const clamp = (x, min, max) => Math.max(min, Math.min(x, max));
 
